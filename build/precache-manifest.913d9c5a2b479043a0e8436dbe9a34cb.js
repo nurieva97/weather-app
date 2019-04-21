511c78715d22fdb4e92e815277c255e8867263c2
Gulshat <nurieva.gulshat.97@mail.ru>
2019-04-21 23:23:45 +0300

    build

---
+++ build/precache-manifest.913d9c5a2b479043a0e8436dbe9a34cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "85c0c6eb9681e2b5eb61",
+    "url": "/static/js/main.dd66df1f.chunk.js"
+  },
+  {
+    "revision": "c45d5c3dabeaf0e57873",
+    "url": "/static/js/2.842aa8e0.chunk.js"
+  },
+  {
+    "revision": "85c0c6eb9681e2b5eb61",
+    "url": "/static/css/main.a679aad3.chunk.css"
+  },
+  {
+    "revision": "fcf09b0ba1c67919c1afe3dba77d5980",
+    "url": "/index.html"
+  }
+];
